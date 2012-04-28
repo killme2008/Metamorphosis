98106fcde53657b725944d680e3a69b47eb14818
dennis zhuang <killme2008@gmail.com>
2012-04-29 02:04:46 +0800

    Added meta-python to contrib

---
+++ contrib/python/meta-python/performance.py
@@ -0,0 +1,14 @@
+from metamorphosis import Message,MessageProducer
+from time import time
+p=MessageProducer("avos-fetch-tasks")
+message=Message("avos-fetch-tasks","http://www.taobao.com")
+start=time()
+for i in range(0,10000):
+    sent=p.send(message)
+    if not sent.success:
+        print "send failed"
+finish=time()
+secs=finish-start
+print "duration:%s seconds" % (secs)
+print "tps:%s msgs/second" % (10000/secs)
+p.close()
