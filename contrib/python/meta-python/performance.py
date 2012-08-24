5152a643f598d2a09ed19483c51724aef52daea8
dennis zhuang <killme2008@gmail.com>
2012-08-24 19:13:07 +0800

    Update performance test in meta-py

--- contrib/python/meta-python/performance.py
+++ contrib/python/meta-python/performance.py
@@ -1,6 +1,9 @@
+#duration:1.90639591217 seconds
+#tps:5245.50012731 msgs/second
+
 from metaq.producer import Message,MessageProducer
 from time import time
-p=MessageProducer("avos-fetch-tasks")
+p=MessageProducer("avos-fetch-tasks",zk_root="/avos-fetch-meta")
 message=Message("avos-fetch-tasks","http://www.taobao.com")
 start=time()
 for i in range(0,10000):
