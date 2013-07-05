f4b7b6dd5a9f716d1965b17cd7be93a58fcd9b89
dennis zhuang <killme2008@gmail.com>
2013-07-06 01:18:38 +0800

    Deprecate com.taobao.metamorphosis.client.consumer.MessageIterator

--- metamorphosis-client/src/main/java/com/taobao/metamorphosis/client/consumer/MessageIterator.java
+++ metamorphosis-client/src/main/java/com/taobao/metamorphosis/client/consumer/MessageIterator.java
@@ -1,21 +1,4 @@
-/*
- * (C) 2007-2012 Alibaba Group Holding Limited.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * Authors:
- *   wuhua <wq163@163.com> , boyan <killme2008@gmail.com>
- */
-package com.taobao.metamorphosis.consumer;
+package com.taobao.metamorphosis.client.consumer;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
@@ -30,9 +13,13 @@ import com.taobao.metamorphosis.utils.MessageUtils;
  * 消息迭代器，解析传输过来的数据
  * 
  * @author boyan
+ * @deprecated Please use com.taobao.metamorphosis.consumer.MessageIterator
+ *             instead.
+ * @see com.taobao.metamorphosis.consumer.MessageIterator
  * @Date 2011-4-20
  * 
  */
+@Deprecated
 public class MessageIterator {
     private final String topic;
     private final byte[] data;
