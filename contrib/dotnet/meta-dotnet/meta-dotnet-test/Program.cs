a25ff37439781bc35f6639ab5ca49e13ce5ee086
kimmking <kimmking.cn@gmail.com>
2013-09-18 16:06:19 +0800

    add meta-dotnet

---
+++ contrib/dotnet/meta-dotnet/meta-dotnet-test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace meta_dotnet_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
